fd7a4336c2f988e49bf5c6c1fa28970a7ea607ba
BoseV <46490582+BoseVJeff@users.noreply.github.com>
2024-09-23 12:16:57 +0530

    Added indexed array exercise in php

---
+++ php/array_index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Array Index</title>
+</head>
+<body>
+    <?php
+    // First way
+    $name=array("a1","b1","c1");
+    echo $name[0]."<br>";
+    echo $name[1]."<br>";
+    echo $name[2]."<br>";
+
+    // Second Way
+    $rno[0]=101;
+    $rno[1]=102;
+    $rno[2]=103;
+
+    echo $rno[0]."<br>";
+    echo $rno[1]."<br>";
+    echo $rno[2]."<br>";
+    ?>
+</body>
+</html>
